eb59c3e12dbee3f4ee4844855d3ecb83adaa34a6
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-08-29 01:26:36 +0200

    motorola: add charge_only_mode to common
    tested on some motorola devices, cliq xt 806, defy..
    
    optional package name is moto_charge_only_mode
    
    Change-Id: I88df18f4af9eafe64c641547716b851585f37551

---
+++ charge_only_mode/Android.mk
@@ -0,0 +1,32 @@
+# Copyright 2011 The Android Open Source Project
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	alarm.c \
+	draw.c \
+	events.c \
+	hardware.c \
+	screen.c \
+	main.c
+
+LOCAL_STATIC_LIBRARIES := libunz libcutils libc
+
+LOCAL_C_INCLUDES := external/zlib
+
+LOCAL_MODULE := moto_charge_only_mode
+LOCAL_MODULE_STEM := charge_only_mode
+
+# Bits per pixels
+LOCAL_CFLAGS += -DFB_BPP=16
+
+# Device screen size
+LOCAL_CFLAGS += -DFB_WIDTH=480 -DFB_HEIGHT=854
+
+# Battery position
+LOCAL_CFLAGS += -DPNG_LEFT=105 -DPNG_TOP=71
+LOCAL_CFLAGS += -DPNG_BOTTOM=318
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
