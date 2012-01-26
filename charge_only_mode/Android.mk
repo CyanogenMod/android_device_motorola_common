b6ba8bb0427643448339799894ffc14f8e2b174a
Nicholas Semendyaev <agent00791@gmail.com>
2012-01-26 16:51:49 +0700

    fix LOCAL_MODULE_PATH for board with bootmenu.

--- charge_only_mode/Android.mk
+++ charge_only_mode/Android.mk
@@ -18,7 +18,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE:= charge_only_mode
 
 # default is /system/bin or /sbin in boot.img
-ifeq ($(BOARD_USES_BOOTMENU), true)
+ifneq ($(BOARD_USES_BOOTMENU), true)
 
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
 LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
