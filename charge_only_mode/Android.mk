edf2c8dd691d68227f5bde5429d02a2405d13778
peshovec <jlesev@gmail.com>
2012-02-22 15:27:54 +0200

    some devices (xt720, probably milestone ) doesn't have lights module they controll the lights and brightness via /sys/class
    modifying hardware.c is just enough to implement the functionality
    
    here is some overcomplicated test-case
    https://github.com/CyanogenModXT720/android_device_motorola_common/commit/e5c060d8639bf4b2135d1d9c8a7fe49d1c512103
    
    Change-Id: I5e8d1ee1ba888260f58bc4785389a4523d923b74

--- charge_only_mode/Android.mk
+++ charge_only_mode/Android.mk
@@ -7,9 +7,15 @@ LOCAL_SRC_FILES:= \
 	alarm.c \
 	draw.c \
 	events.c \
-	hardware.c \
 	screen.c \
 	main.c
+
+ifneq ($(BOARD_CUSTOM_LIGHTS_CONTROL),)
+  LOCAL_SRC_FILES += $(BOARD_CUSTOM_LIGHTS_CONTROL)
+else
+  LOCAL_SRC_FILES += hardware.c
+endif
+
 
 LOCAL_STATIC_LIBRARIES := libunz libcutils libc
 LOCAL_SHARED_LIBRARIES := libhardware
